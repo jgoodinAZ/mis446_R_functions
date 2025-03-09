b6a8d266df9f7ce1fc03c82609c103a80ff1106f
jgoodinAZ <jadegoodin@yahoo.com>
2025-03-08 17:04:11 -0700

    Create FunctionFile.R

---
+++ FunctionFile.R
@@ -0,0 +1,11 @@
+#################################################################
+# User Defined Functions to be used across R script files
+#################################################################
+# Define function to display 2 system variables
+# passed to the function
+display_2SysVars <- function(vSys1, vSys2) {
+  #   Display 1st system variable passed to function
+  print(vSys1)
+  #   Display 2nd system variable passed to function
+  print(vSys2)
+}
